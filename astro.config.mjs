8eb40339782af31e072f9a99a32579e0f54f70ce
Royalprincesingh <princesinghsikata@gmail.com>
2026-03-22 17:59:08 +0530

    Update configurations, components, and add admin pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://princesingh.blog',
+	site: 'https://blog-boss.tech',
 	integrations: [mdx()],
 });
